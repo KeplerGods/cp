1e0a6276a30320b64c1678c0dfd0ebceb7396929
AaronLi <ljz1318078262@163.com>
2019-06-05 08:59:22 +0800

    no message

---
+++ src/main/java/com/ai8s/cp/CPApp.java
@@ -0,0 +1,23 @@
+/**
+ *  @描述：
+ *  
+ *  版本    作者        时间           邮箱                描述
+ * =============================================================
+ * v1.0   李金泽   2019年6月2日  lijinze@beixing360.com     初版
+ * =============================================================
+ * */
+package com.ai8s.cp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Aaron
+ *
+ */
+@SpringBootApplication
+public class CPApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CPApp.class, args);
+	}
+}
